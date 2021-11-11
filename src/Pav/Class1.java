b4c1277bc46e04bdad6fe5479787fad51d3c058e
arshathmichael <arshathmichael12@gmail.com>
2021-11-11 22:03:49 +0530

    Success

---
+++ src/Pav/Class1.java
@@ -0,0 +1,18 @@
+package Pav;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class1 {
+	
+public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arshath Michael\\eclipse-workspace\\Java\\Driver 95\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://www.facebook.com/");
+	
+	}
+
+}
---
+++ src/Pav/Class3.java
@@ -0,0 +1,17 @@
+package Pav;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class3 {
+
+public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arshath Michael\\eclipse-workspace\\Java\\Driver 95\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://www.facebook.com/");
+	
+	}
+}
